c01eac066e266535f529a51c3c6d79c835972630
Kacper Połuszejko <kacperpoluszejko@wp.pl>
2025-06-03 18:17:26 +0200

    update lab 10

--- FUZ_9/fuz_9_v2.py
+++ FUZ_9/fuz_9_v2.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Parametry
-N = 10000
+N = 100
 m0 = 5
 m = 4
 
-A = {i: [] for i in range(N)}  
+A = {i: [] for i in range(N)}
 
 for i in range(m0):
-    for j in range(i + 1, m0):
+    for j in range(i+1, m0):
         A[i].append(j)
         A[j].append(i)
 
@@ -24,20 +24,20 @@ for new_node in range(m0, N):
     total_weight = sum(weights)
 
 # targety to wierzchołki które będą dołączone
-    while len(targets) < m:
-        r = random.uniform(0, total_weight)
-        cumulative = 0
-
+    while(len(targets) < m):
+        U = random.uniform(0, total_weight)
+        suma = 0
         for node, weight in zip(nodes, weights):
-            cumulative += weight
-            if r < cumulative:
+            suma += weight
+            if(U<suma):
                 if node not in targets:
                     targets.append(node)
-                break 
-
+                break
+    
     for target in targets:
-        A[new_node].append(target)
         A[target].append(new_node)
+        A[new_node].append(target)
+
 
 gr = nx.Graph()
 gr.add_nodes_from(range(N))
@@ -45,12 +45,12 @@ for i, v in A.items():
     for j in v:
         gr.add_edge(i, j)
 
-# ZADANIE 1 (rysowanie grafu o N = 100 wierzchołkach)
-# nx.draw(gr, node_size=50, with_labels=False)
-# plt.title("Graf Barabásiego–Alberta (m₀=5, m=4, N=100)")
-# plt.show()
+#Zadanie 1
+nx.draw(gr, node_size=50, with_labels=False)
+plt.savefig("Zad1.png")
+plt.show()
 
-#ZADANIE 2 - rozkład stopni
+#Zadanie 2
 N = len(A)
 
 rank_dist = np.zeros(N)
@@ -63,29 +63,18 @@ rank_dist /= (N - 1)
 
 rank_table = np.arange(0, N)
 nonzero = rank_dist > 0
+rank_table = rank_table[nonzero]
+rank_dist = rank_dist[nonzero]
 
-k_values = rank_table[nonzero]
-P_k = rank_dist[nonzero]
+log_rank_table = np.log10(rank_table)
+log_rank_dist = np.log10(rank_dist)
 
-log_k = np.log10(k_values)
-log_Pk = np.log10(P_k)
-
-slope, intercept = np.polyfit(log_k[:-30], log_Pk[:-30], 1)
+slope, intercept = np.polyfit(log_rank_table, log_rank_dist, 1)
 alpha = -slope
 
 print(f"Szacowane alfa = {alpha:.3f}")
-plt.figure(figsize=(8,6))
-plt.plot(k_values, P_k, 'o', label="Numerycznie")
-plt.plot(k_values, 10**intercept * k_values**slope, 'r-', label="Dopasowanie")
-
-plt.xscale("log")
-plt.yscale("log")
+plt.plot(log_rank_table, log_rank_dist, 'o')
+plt.plot(log_rank_table, slope*log_rank_table + intercept)
 plt.xlabel("k (stopień wierzchołka)")
 plt.ylabel("P(k)")
-plt.title(f"P(k), alfa = {alpha:.3f}")
-plt.grid(True, which="both", ls="--")
-plt.legend()
-plt.tight_layout()
-plt.savefig("zad2_v2.png")
 plt.show()
-
--- FUZ_9/fuz_9_zad_3.py
+++ FUZ_9/fuz_9_zad_3.py
@@ -1,10 +1,12 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
+import numpy as np
 
 # Parametry
 N = 100
 m = 1  
+m0 = 1
 
 G = nx.barabasi_albert_graph(N, m)
 
@@ -20,10 +22,34 @@ def is_connected(G):
     dfs(0, visited, adj)
     return all(visited)
 
+
 plt.figure(figsize=(6, 6))
 nx.draw(G, node_size=50, with_labels=False)
-plt.title("Graf BA (N=100, m=1)")
 plt.show()
+
+adj = {node: list(G.neighbors(node)) for node in G.nodes}
+N = len(adj)
+
+rank_dist = np.zeros(N)
+
+for i in range(N):
+    rank = len(adj[i])
+    rank_dist[rank] += 1
+
+rank_dist /= (N - 1)
+
+rank_table = np.arange(0, N)
+nonzero = rank_dist > 0
+rank_table = rank_table[nonzero]
+rank_dist = rank_dist[nonzero]
+
+print(rank_dist)
+plt.plot(rank_table, rank_dist, 'o')
+plt.xlabel("Stopień wierzchołka")
+plt.ylabel("P(k)")
+plt.grid()
+plt.show()
+
 
 print("Spójny przed usunięciem krawędzi:", is_connected(G))
 
